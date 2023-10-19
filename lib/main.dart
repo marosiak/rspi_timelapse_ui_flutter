f8f3425ea469ac8165ddb80834803c9a23a656e3
Maciej Rosiak <8932578+marosiak@users.noreply.github.com>
2023-10-20 00:16:34 +0200

    Simplest responsibility added

--- lib/main.dart
+++ lib/main.dart
@@ -36,8 +36,6 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
-
 class _DateInfo extends StatelessWidget {
   DateTime? dateToDisplay;
   String helpText;
@@ -57,10 +55,10 @@ class _DateInfo extends StatelessWidget {
     return Row(
       children: [
         Text(helpText,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12)
-        ),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12)),
         SizedBox(width: 8),
-        Text("${formatDateTime(dateToDisplay)}",
+        Text(
+          "${formatDateTime(dateToDisplay)}",
         )
       ],
     );
@@ -71,7 +69,12 @@ class _MyHomePageState extends State<MyHomePage> {
   final channel = HtmlWebSocketChannel.connect('ws://raspberrypi.local/ws');
   late Timer _timer;
   Map<String, double> _cpuStats = {'User': 0, 'System': 0, 'Idle': 0};
-  Map<String, double> _memoryStats = {'Total': 0, 'Used': 0, 'SwapTotal': 0, 'SwapUsed': 0};
+  Map<String, double> _memoryStats = {
+    'Total': 0,
+    'Used': 0,
+    'SwapTotal': 0,
+    'SwapUsed': 0
+  };
   DateTime? _lastUpdatedAt;
   DateTime? _lastPhotoTakenAt;
 
@@ -90,15 +93,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
       setState(() {
         if (data['lastPhotoTakenAt'] != null) {
-          _lastPhotoTakenAt = DateTime.fromMillisecondsSinceEpoch(data['lastPhotoTakenAt'] * 1000);
-
+          _lastPhotoTakenAt = DateTime.fromMillisecondsSinceEpoch(
+              data['lastPhotoTakenAt'] * 1000);
         }
 
         if (data['stats']['cpu'] != null) {
           _cpuStats = {
-            'User': double.parse(data['stats']['cpu']['User'].toStringAsFixed(2)),
-            'System': double.parse(data['stats']['cpu']['System'].toStringAsFixed(2)),
-            'Idle': double.parse(data['stats']['cpu']['Idle'].toStringAsFixed(2)),
+            'User':
+                double.parse(data['stats']['cpu']['User'].toStringAsFixed(2)),
+            'System':
+                double.parse(data['stats']['cpu']['System'].toStringAsFixed(2)),
+            'Idle':
+                double.parse(data['stats']['cpu']['Idle'].toStringAsFixed(2)),
           };
           shouldUpdateDate = true;
         }
@@ -106,15 +112,20 @@ class _MyHomePageState extends State<MyHomePage> {
         if (data['stats']['memory'] != null) {
           _memoryStats = {
             'Total': double.parse(
-                (data['stats']['memory']['Total'] / (1024 * 1024)).toStringAsFixed(2)),
+                (data['stats']['memory']['Total'] / (1024 * 1024))
+                    .toStringAsFixed(2)),
             'Used': double.parse(
-                (data['stats']['memory']['Used'] / (1024 * 1024)).toStringAsFixed(2)),
-            'SwapTotal': double.parse(
-                ((data['stats']['memory']['SwapTotal'] ?? data['stats']['memory']['VirtualTotal']) / (1024 * 1024)).toStringAsFixed(2)
-            ),
-            'SwapUsed': double.parse(
-                ((data['stats']['memory']['SwapUsed'] ?? (data['stats']['memory']['VirtualTotal'] - data['stats']['memory']['VirtualFree'])) / (1024 * 1024)).toStringAsFixed(2)
-            ),
+                (data['stats']['memory']['Used'] / (1024 * 1024))
+                    .toStringAsFixed(2)),
+            'SwapTotal': double.parse(((data['stats']['memory']['SwapTotal'] ??
+                        data['stats']['memory']['VirtualTotal']) /
+                    (1024 * 1024))
+                .toStringAsFixed(2)),
+            'SwapUsed': double.parse(((data['stats']['memory']['SwapUsed'] ??
+                        (data['stats']['memory']['VirtualTotal'] -
+                            data['stats']['memory']['VirtualFree'])) /
+                    (1024 * 1024))
+                .toStringAsFixed(2)),
           };
           shouldUpdateDate = true;
         }
@@ -137,34 +148,52 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null,
-      body: _lastUpdatedAt != null ? Padding(
-        padding: EdgeInsets.all(18.0),
-        child: Column(
-          children: [
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    _DateInfo(dateToDisplay: _lastUpdatedAt, helpText: "Data updated at:"),
-                    SizedBox(height: 4),
-                    _DateInfo(dateToDisplay: _lastPhotoTakenAt, helpText: "Last photo taken at:"),
-                  ],
-                ),
+      body: _lastUpdatedAt != null
+          ? Padding(
+              padding: EdgeInsets.all(18.0),
+              child: Column(
+                children: [
+                  ConstrainedBox(
+                    constraints: BoxConstraints(minWidth: 200),
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            _DateInfo(
+                                dateToDisplay: _lastUpdatedAt,
+                                helpText: "Data updated at:"),
+                            SizedBox(height: 4),
+                            _DateInfo(
+                                dateToDisplay: _lastPhotoTakenAt,
+                                helpText: "Last photo taken at:"),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Wrap(
+                    alignment: WrapAlignment.spaceBetween,
+                    children: [
+                      SizedBox(height: 8),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(minWidth: 200, maxWidth: 600),
+                        child: CpuStatsWidget(cpuData: _cpuStats),
+                      ),
+                      SizedBox(
+                        width: 48,
+                      ),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(minWidth: 400, maxWidth: 600),
+                        child: MemoryStatsWidget(memData: _memoryStats),
+                      ),
+                    ],
+                  )
+                ],
               ),
-            ),
-            SizedBox(height: 8),
-
-            Row(
-              children: [
-                CpuStatsWidget(cpuData: _cpuStats),
-                SizedBox(width: 48,),
-                MemoryStatsWidget(memData: _memoryStats),
-              ],
-            ),
-          ],
-        ),
-      ) : Container(),
+            )
+          : Container(),
     );
   }
 }
